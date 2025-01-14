a9430d54bbc1cfce3bb5100a7972434e85d35063
NOORFATINI FARHAH BINTI NOOR ADLY <noorfatinifarhah@graduate.utm.my>
2025-01-15 05:27:35 +0800

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.ProductionPlan)
+
+class ProductionPlanAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'username',]
+
+admin.site.register(models.ProductionPlan, ProductionPlanAdmin)
